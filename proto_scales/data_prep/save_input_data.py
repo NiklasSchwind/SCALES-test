306a8b610610f14978ef2ab8d9540114db1b82b3
Verena Kain <verena.kain@cern.ch>
2026-02-23 13:24:29 +0100

    scenario ssp245 with 3 training datasets

--- proto_scales/data_prep/save_input_data.py
+++ proto_scales/data_prep/save_input_data.py
@@ -902,7 +902,7 @@ def shuffle_train_data(X, Y, random_state=None):
 
 MODEL = 'ACCESS-ESM1-5'
 INDICATOR = 'tas'#'pr' #'tas'
-TEST_SCENARIOS = ['ssp585']
+TEST_SCENARIOS = ['ssp245']
 #TRAIN_SCENARIOS = ['ssp585','1pctco2','ssp245'] # for "pr"
 monthly_flag = True
 use_smoothing = False
