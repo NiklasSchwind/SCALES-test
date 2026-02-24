daa9405659e896dd9d136aecbc7afc72afefab46
Verena Kain <verena.kain@cern.ch>
2026-02-24 17:04:37 +0100

    bug fix

--- proto_scales/train_scales_ssm_z2pr.py
+++ proto_scales/train_scales_ssm_z2pr.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
   
     device = "cuda"
 
-    model, y_scaler, u_scaler,pr_scaler = scales_ssm_ridge.run_train(tas, pr,u, context_len=100, z_dim=24,horizon=100, device=device,epochs=10,batch_size=250)
+    model, y_scaler, u_scaler,pr_scaler = scales_ssm_z2pr.run_train(tas, pr,u, context_len=100, z_dim=24,horizon=100, device=device,epochs=10,batch_size=250)
     os.makedirs("outputs_ssm_scales", exist_ok=True)
     torch.save(model.state_dict(),"outputs_ssm_scales/model_out")
     y_scaler.save("outputs_ssm_scales/y_scaler.out")
